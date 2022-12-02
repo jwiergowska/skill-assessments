60df1b1734dc8d4b0e93f89b8bf1c5dc53832dd4
jwiergowska <85780659+jwiergowska@users.noreply.github.com>
2022-12-02 19:40:52 +0100

    Update tic_tac_toe.py

--- Python Programming/tic_tac_toe.py
+++ Python Programming/tic_tac_toe.py
@@ -1,18 +1,10 @@
+# > 1. When the user selects "X", the computer ("O") plays two moves for every one move the user makes:
+
+# I fixed this and instead of 'player', the code displays 'X' at the end of the code where the major bug was.
+
 import random
 
-def drawBoard(board: list) -> None:
-    """
-    This function prints out the board.
-    :board: list of strings representing the board (ignore index 0).
-    """
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[1] + '|' + board[2] + '|' + board[3])
-
-
-def inputPlayerLetter() -> list:
+def askForLetterr() -> list:
     """
     The function choses if player wants to be X or O. 
     
@@ -24,32 +16,56 @@ def inputPlayerLetter() -> list:
         print('X or O? ')
         letter = input().upper()
 
-    # The first element in the list is the player's letter; the second is the computer's letter.
+    # The first element is the player, the second is the computer.
     if letter == 'X':   
         return ['X', 'O']
     else:   
         return ['O', 'X']
 
+def printBoard(board: list) -> None:
+    """
+    This function prints out the board.
+
+    :board: list of strings representing the board (without index 0).
+    """
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+def printBoard(board: list) -> None:
+    """
+    This function prints out the board.
+
+    :board: list of strings representing the board (without index 0).
+    """
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
 def makeMove(board, letter, move):
     board[move] = letter
 
-def isWinner(bo, le):
+def isWinner(b, l):
     """
     Given a board and a player's letter, this function returns True if that player won.
     
-    :bo: board
-    :le: letter.
+    :b: board
+    :l: letter.
     """
-    return ((bo[7] == le and bo[8] == le and bo[9] == le) or # Across the top
-          (bo[4] == le and bo[5] == le and bo[6] == le) or # Across the middle
-          (bo[1] == le and bo[2] == le and bo[3] == le) or # Across the bottom
-          (bo[7] == le and bo[4] == le and bo[1] == le) or # Down the left side
-          (bo[8] == le and bo[5] == le and bo[2] == le) or # Down the middle
-          (bo[9] == le and bo[6] == le and bo[3] == le) or # Down the right side
-          (bo[7] == le and bo[5] == le and bo[3] == le) or # Diagonal
-          (bo[9] == le and bo[5] == le and bo[1] == le)) # Diagonal
+    return ((b[7] == l and b[8] == l and b[9] == l) or # top
+          (b[4] == l and b[5] == l and b[6] == l) or # middle
+          (b[1] == l and b[2] == l and b[3] == l) or # bottoms
+          (b[7] == l and b[4] == l and b[1] == l) or # left side
+          (b[8] == l and b[5] == l and b[2] == l) or # middle
+          (b[9] == l and b[6] == l and b[3] == l) or # right side
+          (b[7] == l and b[5] == l and b[3] == l) or # diagonal
+          (b[9] == l and b[5] == l and b[1] == l)) # diagonald
 
-def getBoardCopy(board):
+def copyBoard(board):
     """
     Making a copy of the board list and return it.
     """
@@ -74,7 +90,7 @@ def getPlayerMove(board):
         move = input()
     return int(move)
 
-def chooseRandomMoveFromList(board, movesList):
+def chooseRandomMove(board, movesList):
     """
     Returns a valid move from the passed list on the passed board.
     Returns None if there is no valid move.
@@ -89,44 +105,6 @@ def chooseRandomMoveFromList(board, movesList):
     else:
         return None
 
-def getComputerMove(board, computerLetter):
-    """
-    Given a board and the computer's letter, determine where to move and return that move.
-    """
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
-
-    # Here is the algorithm for our tictactoeJulka AI:
-    # First, check if we can win in the next move.
-    for i in range(1, 10):
-        boardCopy = getBoardCopy(board)
-        if isSpaceFree(boardCopy, i):
-            makeMove(boardCopy, computerLetter, i)
-            if isWinner(boardCopy, computerLetter):
-                return i
-
-   # Check if the player could win on their next move and block them.
-    for i in range(1, 10):
-        boardCopy = getBoardCopy(board)
-        if isSpaceFree(boardCopy, i):
-            makeMove(boardCopy, playerLetter, i)
-            if isWinner(boardCopy, playerLetter):
-                return i
-
-    # Try to take one of the corners, if they are free.
-    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
-    if move != None:
-        return move
-
-    # Try to take the center, if it is free.
-    if isSpaceFree(board, 5):
-        return 5
-
-    # Move on one of the sides.
-    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
-
 def isBoardFull(board):
     """
     Return True if every space on the board has been taken. Otherwise, return False.
@@ -136,119 +114,167 @@ def isBoardFull(board):
             return False
     return True
 
+def juliaAiBot(board, computerLetter):
+    """
+   Choosing which move is the most optial and playing till the game is finished. 
+   Each time printing the board, also asking for a letter and asking if the player wants to play again.
+
+   :board: given board
+   :computerLetter: the letter which is attributed to computer.
+    """
+    if computerLetter == 'X':
+        playerLetter = 'O'
+    else:
+        playerLetter = 'X'
+
+    # Here is Julia's AI bot:
+    # Checking, who can win in one move.
+    for i in range(1, 10):
+        boardCopy = copyBoard(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, computerLetter, i)
+            if isWinner(boardCopy, computerLetter):
+                return i
+
+   # Checking if the player could win in the next move, if so, block them.
+    for i in range(1, 10):
+        boardCopy = copyBoard(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, playerLetter, i)
+            if isWinner(boardCopy, playerLetter):
+                return i
+
+    # Taking corners (if free).
+    move = chooseRandomMove(board, [1, 3, 7, 9])
+    if move != None:
+        return move
+
+    # Taking the center (if free).
+    if isSpaceFree(board, 5):
+        return 5
+
+    # Move on one of the sides.
+    return chooseRandomMove(board, [2, 4, 6, 8])
 
 while True:
     # Reset the board.
-    theBoard = [' '] * 10
-    playerLetter, computerLetter = inputPlayerLetter()
+    ourBoard = [' '] * 10
+    playerLetter, computerLetter = askForLetterr()
     turn = playerLetter
     if turn == "X":
         print('The player will go first.')
     else:
-        print('The computer will go first.')
+        print("Julia's AI will go first.")
     gameIsPlaying = True
 
     while gameIsPlaying:
         if turn == "X":
             # Player's turn
-            drawBoard(theBoard)
-            move = getPlayerMove(theBoard)
-            makeMove(theBoard, playerLetter, move)
+            printBoard(ourBoard)
+            move = getPlayerMove(ourBoard)
+            makeMove(ourBoard, playerLetter, move)
 
-            if isWinner(theBoard, playerLetter):
-                drawBoard(theBoard)
+            if isWinner(ourBoard, playerLetter):
+                printBoard(ourBoard)
                 print('Yeah! You have won the game!')
                 gameIsPlaying = False
+                break
             else:
-                if isBoardFull(theBoard):
-                    drawBoard(theBoard)
+                if isBoardFull(ourBoard):
+                    printBoard(ourBoard)
                     print('The game is a tie!')
                     break
                 else:
                     turn = 'computer'
             if turn == 'player':
                 # Player's turn
-                drawBoard(theBoard)
-                move = getPlayerMove(theBoard)
-                makeMove(theBoard, playerLetter, move)
+                printBoard(ourBoard)
+                move = getPlayerMove(ourBoard)
+                makeMove(ourBoard, playerLetter, move)
 
-                if isWinner(theBoard, playerLetter):
-                    drawBoard(theBoard)
+                if isWinner(ourBoard, playerLetter):
+                    printBoard(ourBoard)
                     print('Yeah! You have won the game!')
                     gameIsPlaying = False
+                    break
                 else:
-                    if isBoardFull(theBoard):
-                        drawBoard(theBoard)
+                    if isBoardFull(ourBoard):
+                        printBoard(ourBoard)
                         print('The game is a tie!')
                         break
                     else:
                          turn = 'computer'
             else:
                 # Computer's turn
-                move = getComputerMove(theBoard, computerLetter)
-                makeMove(theBoard, computerLetter, move)
+                move = juliaAiBot(ourBoard, computerLetter)
+                makeMove(ourBoard, computerLetter, move)
 
-                if isWinner(theBoard, computerLetter):
-                    drawBoard(theBoard)
-                    print('The computer won.')
+                if isWinner(ourBoard, computerLetter):
+                    printBoard(ourBoard)
+                    print("Julia's AI won.")
                     gameIsPlaying = False
+                    break
                 else:
-                    if isBoardFull(theBoard):
-                        drawBoard(theBoard)
+                    if isBoardFull(ourBoard):
+                        printBoard(ourBoard)
                         print('The game is a tie!')
                         break
                     else:
-                        turn = 'player'
+                        turn = 'X'
         else:
             # Computer's turn
-            move = getComputerMove(theBoard, computerLetter)
-            makeMove(theBoard, computerLetter, move)
+            move = juliaAiBot(ourBoard, computerLetter)
+            makeMove(ourBoard, computerLetter, move)
 
-            if isWinner(theBoard, computerLetter):
-                drawBoard(theBoard)
-                print('The computer won.')
+            if isWinner(ourBoard, computerLetter):
+                printBoard(ourBoard)
+                print("Julia's AI won.")
                 gameIsPlaying = False
+                break
             else:
-                if isBoardFull(theBoard):
-                    drawBoard(theBoard)
+                if isBoardFull(ourBoard):
+                    printBoard(ourBoard)
                     print('The game is a tie!')
                     break
                 else:
                     turn = 'player'
             if turn == 'player':
                 # Player's turn
-                drawBoard(theBoard)
-                move = getPlayerMove(theBoard)
-                makeMove(theBoard, playerLetter, move)
+                printBoard(ourBoard)
+                move = getPlayerMove(ourBoard)
+                makeMove(ourBoard, playerLetter, move)
 
-                if isWinner(theBoard, playerLetter):
-                    drawBoard(theBoard)
+                if isWinner(ourBoard, playerLetter):
+                    printBoard(ourBoard)
                     print('Yeah! You have won the game!')
                     gameIsPlaying = False
+                    break
                 else:
-                    if isBoardFull(theBoard):
-                        drawBoard(theBoard)
+                    if isBoardFull(ourBoard):
+                        printBoard(ourBoard)
                         print('The game is a tie!')
                         break
                     else:
                          turn = 'computer'
             else:
                 # Computer's turn
-                move = getComputerMove(theBoard, computerLetter)
-                makeMove(theBoard, computerLetter, move)
+                move = juliaAiBot(ourBoard, computerLetter)
+                makeMove(ourBoard, computerLetter, move)
 
-                if isWinner(theBoard, computerLetter):
-                    drawBoard(theBoard)
-                    print('The computer won.')
+                if isWinner(ourBoard, computerLetter):
+                    printBoard(ourBoard)
+                    print("Julia's AI won.")
                     gameIsPlaying = False
+                    break
                 else:
-                    if isBoardFull(theBoard):
-                        drawBoard(theBoard)
+                    if isBoardFull(ourBoard):
+                        printBoard(ourBoard)
                         print('The game is a tie!')
                         break
                     else:
                         turn = 'player'
 
-    print('Do you want to play again? (yes or no)') 
+    print('Would you like to play again? (yes or no)') 
     if not input().lower().startswith('y'):
         break
+        ```
