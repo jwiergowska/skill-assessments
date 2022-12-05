b306617b6c56a8e19f593dbbd0f4ff1f0cfbe466
jwiergowska <85780659+jwiergowska@users.noreply.github.com>
2022-12-05 07:05:51 +0100

    Update tic_tac_toe.py

--- Python Programming/tic_tac_toe.py
+++ Python Programming/tic_tac_toe.py
@@ -282,3 +282,5 @@ while True:
         # https://medium.com/byte-tales/the-classic-tic-tac-toe-game-in-python-3-1427c68b8874, 
         # https://inventwithpython.com/invent4thed/chapter10.html,
         # https://github.com/kying18/tic-tac-toe
+        # https://github.com/watsojar/tictactoe
+        
